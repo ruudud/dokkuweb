cbba548fe13c034a189ac40472c229a5443cda9b
Pål Ruud <ruudud@gmail.com>
2014-01-26 14:55:32 +0100

    Refactor XHR utils, remove code duplication

--- static/js/xhr_utils.js
+++ static/js/xhr_utils.js
@@ -1,44 +1,34 @@
-// Source: http://www.html5rocks.com/en/tutorials/es6/promises/
-(function (xhr) {
-  xhr.get = function (url) {
-    return new Promise(function(resolve, reject) {
-      var req = new XMLHttpRequest();
-      req.open('GET', url);
+// Inspiration: http://www.html5rocks.com/en/tutorials/es6/promises/
+(function(xhr) {
 
-      req.onload = function() {
-        // This is called even on 404 etc so check the status
-        if (req.status == 200) {
-          // Resolve the promise with the response text
-          resolve(req.response);
-        }
-        else {
-          // Otherwise reject with the status text
-          // which will hopefully be a meaningful error
-          reject(Error(req.statusText));
-        }
-      };
-
-      // Handle network errors
-      req.onerror = function() {
-        reject(Error("Network Error"));
-      };
-
-      req.send();
-    });
-  };
-
-  xhr.getJSON = function (url) {
+  xhr.getJSON = function(url) {
     return xhr.get(url).then(JSON.parse);
   };
 
-  xhr.postWithJSONResponse = function (url, data) {
+  xhr.postWithJSONResponse = function(url, data) {
     return xhr.post(url, data).then(JSON.parse);
   };
 
-  xhr.post = function (url, data) {
+  xhr.put = function(url, data) {
+    return xhr.request('PUT', url, data);
+  };
+
+  xhr.delete = function(url, data) {
+    return xhr.request('DELETE', url, data);
+  };
+
+  xhr.get = function(url) {
+    return xhr.request('GET', url);
+  };
+
+  xhr.post = function(url, data) {
+    return xhr.request('POST', url, data);
+  };
+
+  xhr.request = function(method, url, data) {
     return new Promise(function(resolve, reject) {
       var req = new XMLHttpRequest();
-      req.open('POST', url);
+      req.open(method, url);
 
       req.onload = function() {
         // This is called even on 404 etc so check the status
@@ -61,6 +51,5 @@
       req.send(data);
     });
   };
-
 
 })(window.xhr = window.xhr || {})
